d9caa15b10750a3dbdd3c399c9d3831a9918e628
Gedeon00 <silvan.traxel@gmx.de>
2022-06-26 21:08:30 +0200

    seperated motor pins

--- Adafruit_Video_Looper/action.py
+++ Adafruit_Video_Looper/action.py
@@ -1,7 +1,9 @@
 import RPi.GPIO as GPIO
 import time
 
-motor = 8
+motor_open = 8
+motor_close = 10
+
 close_time = 3
 
 GPIO.setmode(GPIO.BOARD)
@@ -9,14 +11,14 @@ GPIO.setup(motor, GPIO.OUT)
 
 def open_curtain():
     # open curtain
-    GPIO.output(motor, GPIO.HIGH)
+    GPIO.output(motor_open, GPIO.HIGH)
     print("opening curtain")
     time.sleep(close_time)
-    GPIO.output(motor, GPIO.LOW)
+    GPIO.output(motor_open, GPIO.LOW)
 
 def close_curtain():
     # close curtain
-    GPIO.output(motor, GPIO.HIGH)
+    GPIO.output(motor_close, GPIO.HIGH)
     print("closing curtain")
     time.sleep(close_time)
-    GPIO.output(motor, GPIO.LOW)
+    GPIO.output(motor_close, GPIO.LOW)
